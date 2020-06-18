c40ae09ad14d4db721530cd992cb65754899db6d
Chuckz Okoye <tricelex@gmail.com>
2020-06-18 19:04:38 +0100

    created login function ad views

--- django_todo/urls.py
+++ django_todo/urls.py
@@ -19,9 +19,12 @@ from todo import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
 
     # Auth
     path('signup/', views.signupuser, name='signupuser'),
+    path('logout/', views.logoutuser, name='logoutuser'),
+    path('login/', views.loginuser, name='loginuser'),
 
     # Todos
     path('current/', views.currenttodos, name='currenttodos'),
--- todo/views.py
+++ todo/views.py
@@ -1,14 +1,19 @@
 from django.db import IntegrityError
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
-from django.contrib.auth import login
+from django.contrib.auth import login, logout, authenticate
 
 
 # Create your views here.
+
+def home(request):
+    return render(request, 'todo/home.html')
+
+
 def signupuser(request):
     if request.method == 'GET':
-        return render(request, 'todo/signupuser.html', {'form': UserCreationForm()})
+        return render(request, 'todo/login.html', {'form': UserCreationForm()})
     else:
         # create a new user
         if request.POST['password1'] == request.POST['password2']:
@@ -29,3 +34,22 @@ def signupuser(request):
 
 def currenttodos(request):
     return render(request, 'todo/currenttodos.html')
+
+
+def logoutuser(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('home')
+
+
+def loginuser(request):
+    if request.method == 'GET':
+        return render(request, 'todo/login.html', {'form': AuthenticationForm()})
+    else:
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            return render(request, 'todo/login.html',
+                          {'form': AuthenticationForm(), 'error': 'Username and password do not match'})
+        else:
+            login(request, user)
+            return redirect('currenttodos')
