46ab7a0150cdea835d84bb918542d42723aa9731
Chuckz Okoye <tricelex@gmail.com>
2020-06-18 20:40:33 +0100

    created form for todo create view

--- todo/views.py
+++ todo/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.contrib.auth import login, logout, authenticate
+from .forms import TodoForm
 
 
 # Create your views here.
@@ -53,3 +54,17 @@ def loginuser(request):
         else:
             login(request, user)
             return redirect('currenttodos')
+
+
+def create(request):
+    if request.method == 'GET':
+        return render(request, 'todo/create.html', {'form': TodoForm()})
+    else:
+        try:
+            form = TodoForm(request.POST)
+            newtodo = form.save(commit=False)
+            newtodo.user = request.user
+            newtodo.save()
+            return redirect('currenttodos')
+        except ValueError:
+            return render(request, 'todo/create.html', {'form': TodoForm(), 'error': 'Bad data passed in. Try again'})
